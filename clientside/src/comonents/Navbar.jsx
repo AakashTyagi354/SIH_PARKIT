c19230d1774b89e839f551e1e6ae7fb1206c4ed8
Aakash Tyagi <aakashtyagi354@gmail.com>
2023-09-24 08:49:50 +0530

    logo added

--- clientside/src/comonents/Navbar.jsx
+++ clientside/src/comonents/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
+import logo from "../utils/Screenshot_2023-09-23_at_8.28.59_PM-removebg-preview (1).png";
 
-
-import '../styles/styles.css'
+import "../styles/styles.css";
 
 const Navbar = () => {
   return (
-    <div  className='Nav-main'>
-        <div className="logo">
+    <div className="Nav-main">
+      <div className="logo">
+        <img style={{
+          height:"5vh"
+        }} src={logo} alt="" />
+      </div>
+      <div className="pages non-hover">
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/report">Report</a>
+      </div>
+      <div className="log"></div>
+    </div>
+  );
+};
 
-        </div>
-        <div className="pages non-hover">
-          <a href='/home'>Home</a>
-          <a href='/about'>About</a>
-          <a href='/report'>Report</a>
-        </div>
-        <div className="log">
-
-        </div>
-    </div> 
-  )
-}
-
-export default Navbar
+export default Navbar;
